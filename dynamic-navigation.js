378014ac94649752744164c0b3d7562a120da328
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-01 16:15:21 -0400

    Formatting

--- dynamic-navigation.js
+++ dynamic-navigation.js
@@ -1,54 +1,54 @@
 document.addEventListener("DOMContentLoaded", function extractHeadings() {
-	const headings = Array.from(document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]"));
-	const navList = createNavigation(headings);
-	removeRedundantWords(navList);
+    const headings = Array.from(document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]"));
+    const navList = createNavigation(headings);
+    removeRedundantWords(navList);
 
-	document.body.insertBefore(navList, document.body.firstChild);
+    document.body.insertBefore(navList, document.body.firstChild);
 });
 
 function createNavigation(headings) {
-	const navList = document.createElement("ul");
-	navList.classList.add("navbar");
+    const navList = document.createElement("ul");
+    navList.classList.add("navbar");
 
-	let currentList = navList;
-	let lastLevel = 2;
+    let currentList = navList;
+    let lastLevel = 2;
 
-	headings.forEach((heading) => {
-		const level = parseInt(heading.tagName[1]);
+    headings.forEach((heading) => {
+        const level = parseInt(heading.tagName[1]);
 
-		if (level > lastLevel) {
-			const newList = document.createElement("ul");
-			newList.classList.add("has-submenu");
-			currentList.lastElementChild.appendChild(newList);
-			currentList = newList;
-		} else if (level < lastLevel) {
-			const diff = lastLevel - level;
-			currentList = currentList.parentElement.parentElement;
-			for (let i = 1; i < diff; i++) {
-				currentList = currentList.parentElement.parentElement;
-			}
-		}
+        if (level > lastLevel) {
+            const newList = document.createElement("ul");
+            newList.classList.add("has-submenu");
+            currentList.lastElementChild.appendChild(newList);
+            currentList = newList;
+        } else if (level < lastLevel) {
+            const diff = lastLevel - level;
+            currentList = currentList.parentElement.parentElement;
+            for (let i = 1; i < diff; i++) {
+                currentList = currentList.parentElement.parentElement;
+            }
+        }
 
-		const listItem = document.createElement("li");
-		listItem.classList.add("nav-item");
+        const listItem = document.createElement("li");
+        listItem.classList.add("nav-item");
 
-		const link = document.createElement("a");
-		link.classList.add("nav-link");
-		link.textContent = heading.textContent;
-		link.href = `#${heading.id}`;
+        const link = document.createElement("a");
+        link.classList.add("nav-link");
+        link.textContent = heading.textContent;
+        link.href = `#${heading.id}`;
 
-		listItem.appendChild(link);
-		currentList.appendChild(listItem);
+        listItem.appendChild(link);
+        currentList.appendChild(listItem);
 
-		lastLevel = level;
-	});
+        lastLevel = level;
+    });
 
-	return navList;
+    return navList;
 }
 
 function removeRedundantWords(navList) {
-	const links = navList.querySelectorAll(".nav-link");
-	links.forEach((link) => {
-		link.textContent = link.textContent.replace(/(tags|elements?)/gi, "").trim();
-	});
+    const links = navList.querySelectorAll(".nav-link");
+    links.forEach((link) => {
+        link.textContent = link.textContent.replace(/(tags|elements?)/gi, "").trim();
+    });
 }
