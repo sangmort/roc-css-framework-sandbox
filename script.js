03cae329216eb1b59fbb95937c829b27fb138663
sangmort <22422853+sangmort@users.noreply.github.com>
2023-04-30 20:31:11 -0400

    Testing if links are made from headings with IDs

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 "use strict";
 // wait for DOM to load
-document.addEventListener("DOMContentLoaded"),
-	function createNav() {
-		// Grab all headings that have an ID
-		const headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+document.addEventListener("DOMContentLoaded", function createNav() {
+	// Grab all headings that have an ID
+	const headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
 
-		// Create an array of links where the href is the heading ID
-		const links = Array.from(headings).map((heading) => {
-			const link = document.createElement("a");
-			link.href = `#${heading.id}`;
-			link.textContent = heading.textContent;
-			return link;
-		});
-	};
+	// Create an array of links where the href is the heading ID
+	const links = Array.from(headings).map((heading) => {
+		const link = document.createElement("a");
+		link.href = `#${heading.id}`;
+		link.textContent = heading.textContent;
+        console.log(link);
+		return link;
+
+	});
+});
