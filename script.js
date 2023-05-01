01fdebaffbb9c559afe8d994b7a361fdf837baf3
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-01 13:55:17 -0400

    Refactor js to simplify UL/LI tree

--- script.js
+++ script.js
@@ -1,58 +1,36 @@
-"use strict";
-// Wait for DOM to load
-document.addEventListener("DOMContentLoaded", function createNav() {
-	// Grab all headings that have an ID
-	const headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+document.addEventListener("DOMContentLoaded", function () {
+	let headings = document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]");
+	let navList = document.createElement("ul");
+	navList.classList.add("navbar");
 
-	// Create an array of links where the href is the heading ID
-	const headingLink = Array.from(headings).map((heading) => {
-		const link = document.createElement("a");
-		link.href = `#${heading.id}`;
-		link.textContent = heading.textContent;
-		return { level: Number(heading.nodeName.charAt(1)), link };
-	});
-
-	// Add created links to <nav> ID
-	const linksContainer = document.getElementById("html-tag-group-links");
-
-	// Create an unordered list for navigation
-	const navigationList = document.createElement("ul");
-
-	// Add a `nav-menu` class to outermost unordered list
-	navigationList.classList.add("nav-menu");
-	let parentListItems = [navigationList];
-
-	// Loop over each heading link and create a list item for it
-	headingLink.forEach(({ level, link }) => {
-		const navigationItem = document.createElement("li");
-		navigationItem.classList.add("nav-item"); // Add class to list item
-		navigationItem.appendChild(link);
-
-		// Check parent heading level
-		if (level > 1) {
-			const parentListItem = parentListItems[level - 2];
-			if (!parentListItem || !parentListItem.lastChild || parentListItem.lastChild.tagName !== "UL") {
-				const newUl = document.createElement("ul");
-
-				// Add `nav-menu` class to every `ul` element
-				newUl.classList.add("nav-menu");
-
-				if (level === 2) {
-					const newLi = document.createElement("li");
-					newLi.appendChild(newUl);
-					parentListItem.appendChild(newLi);
-				} else {
-					parentListItem.appendChild(newUl);
-				}
-				parentListItems[level - 1] = newUl;
+	let currentList = navList;
+	let lastLevel = 2;
+	for (let i = 0; i < headings.length; i++) {
+		let heading = headings[i];
+		let level = parseInt(heading.tagName[1]);
+		if (level > lastLevel) {
+			let newList = document.createElement("ul");
+			newList.classList.add("has-submenu");
+			currentList.lastElementChild.appendChild(newList);
+			currentList = newList;
+		} else if (level < lastLevel) {
+			let diff = lastLevel - level;
+			for (let j = 0; j < diff; j++) {
+				currentList = currentList.parentElement.parentElement;
 			}
-			parentListItems[level - 1].appendChild(navigationItem);
-		} else {
-			navigationList.appendChild(navigationItem);
-			parentListItems = [navigationList];
 		}
-	});
 
-	// Append navigationList to the <nav> ID
-	linksContainer.appendChild(navigationList);
+		let listItem = document.createElement("li");
+		listItem.classList.add("nav-item");
+		let link = document.createElement("a");
+		link.classList.add("nav-link");
+		link.textContent = heading.textContent;
+		link.href = "#" + heading.id;
+		listItem.appendChild(link);
+		currentList.appendChild(listItem);
+
+		lastLevel = level;
+	}
+
+	document.body.insertBefore(navList, document.body.firstChild);
 });
