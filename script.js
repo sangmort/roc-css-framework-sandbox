09c0ef7f9c47d8945bef428c7b5e4063a3259446
sangmort <22422853+sangmort@users.noreply.github.com>
2023-04-30 20:47:07 -0400

    Complete script that creates <nav> from heading ID

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ document.addEventListener("DOMContentLoaded", function createNav() {
 	const headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
 
 	// Create an array of links where the href is the heading ID
-	const links = Array.from(headings).map((heading) => {
+	const headingLink = Array.from(headings).map((heading) => {
 		const link = document.createElement("a");
 		link.href = `#${heading.id}`;
 		link.textContent = heading.textContent;
-        console.log(link);
+		console.log(link);
 		return link;
+	});
 
+	// Add created links to <nav> ID
+	const linksContainer = document.getElementById("html-tag-group-links");
+
+	// Loop over each heading link and append it to the <nav> ID
+	headingLink.forEach((link) => {
+		linksContainer.appendChild(link);
+		linksContainer.appendChild(document.createElement("br"));
 	});
 });
