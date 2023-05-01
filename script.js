86651b43f8ad1a91b9188fd2140bcbf24a448cf2
sangmort <22422853+sangmort@users.noreply.github.com>
2023-04-30 20:18:35 -0400

    More JS....

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 "use strict";
+// wait for DOM to load
 document.addEventListener("DOMContentLoaded"),
 	function createNav() {
-		// wait for DOM to load
-
+		// Grab all headings that have an ID
 		const headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+
+		// Create an array of links where the href is the heading ID
+		const links = Array.from(headings).map((heading) => {
+			const link = document.createElement(a);
+		});
 	};
