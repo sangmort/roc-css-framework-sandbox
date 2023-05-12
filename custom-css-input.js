f8215a53ebec3cb3f252a01806ae9bd5e978d145
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-12 11:07:28 -0400

    Add buttons & basic JS

--- custom-css-input.js
+++ custom-css-input.js
@@ -1,9 +1,38 @@
-function previewStylesheet() {
-    let stylesheet = document.getElementById("stylesheet").value;
-    let head = document.head;
-    let style = document.createElement("style");
-    style.textContent = stylesheet;
-    head.appendChild(style);
-}
+const fileInput = document.getElementById("file-input");
 
-document.getElementById("preview").addEventListener("click", previewStylesheet);
+fileInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = (event) => {
+        // Store content of CSS file in a variable
+        const css = event.target.result;
+    };
+});
+
+const applyStylesButton = document.getElementById("apply-styles");
+
+applyStylesButton.addEventListener("click", () => {
+    const cssDataUri = "data:text/css;charset=utf-8," + encodeURIComponent(css);
+
+    const customStylesheetLink = document.createElement("link");
+    customStylesheetLink.setAttribute("rel", "stylesheet");
+    customStylesheetLink.setAttribute("href", cssDataUri);
+
+    const head = document.getElementsByTagName("head")[0];
+    head.appendChild(customStylesheetLink);
+});
+
+const removeStylesButton = document.getElementById("remove-styles");
+
+removeStylesButton.addEventListener("click", () => {
+    const customStylesheetLink = document.querySelector(
+        'link[href^="data:text/css"]'
+    );
+
+    if (customStylesheetLink) {
+        customStylesheetLink.parentNode.removeChild(customStylesheetLink);
+    }
+});
