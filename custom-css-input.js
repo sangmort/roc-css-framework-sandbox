c29835723cfe875a9bcb012dd97b14912d3b70d8
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-12 18:38:09 -0400

    Update names to be more semantic

--- custom-css-input.js
+++ custom-css-input.js
@@ -1,21 +1,21 @@
 // Wait for DOM to load before executing user-styling script
 document.addEventListener("DOMContentLoaded", function getUserStyles() {
     // Target the input & create an empty variable to store the css
-    const fileInput = document.getElementById("file-input");
+    const inputUserStylesFile = document.getElementById("input-styles");
     let css;
 
     // Wait for a file to be inputted
-    fileInput.addEventListener("change", (cssInputEvent) => {
+    inputUserStylesFile.addEventListener("change", (inputUserStylesEvent) => {
         // Use first file inputted
-        const file = cssInputEvent.target.files[0];
+        const file = inputUserStylesEvent.target.files[0];
 
         // Call FileReader constructor into variable to read file
         const reader = new FileReader();
         reader.readAsText(file); // Read the file as text
 
         // Trigger FileReader when the file has been read
-        reader.onload = (cssLoadEvent) => {
-            css = cssLoadEvent.target.result; // Store in css variable
+        reader.onload = (loadUserStylesEvent) => {
+            css = loadUserStylesEvent.target.result; // Store in css variable
         };
     });
 
@@ -24,19 +24,19 @@ document.addEventListener("DOMContentLoaded", function getUserStyles() {
 
     // Wait for apply styles but to be clicked
     applyStylesButton.addEventListener("click", function applyUserStyles() {
-        // If css variable is initialized, store contents of inputed file in a data URI
+        // When css variable is initialized, store contents of inputed file in a data URI
         if (css) {
             const cssDataUri =
                 "data:text/css;charset=utf-8," + encodeURIComponent(css);
 
             // Create link rel to the stylesheet ...
-            const customStylesheetLink = document.createElement("link");
-            customStylesheetLink.setAttribute("rel", "stylesheet");
-            customStylesheetLink.setAttribute("href", cssDataUri);
+            const linkUserStyles = document.createElement("link");
+            linkUserStyles.setAttribute("rel", "stylesheet");
+            linkUserStyles.setAttribute("href", cssDataUri);
 
             // ... and append to head element
             const head = document.getElementsByTagName("head")[0];
-            head.appendChild(customStylesheetLink);
+            head.appendChild(linkUserStyles);
         }
     });
 
@@ -45,13 +45,13 @@ document.addEventListener("DOMContentLoaded", function getUserStyles() {
 
     removeStylesButton.addEventListener("click", function removeUserStyles() {
         // Check if link element href starts with "data:text/css"
-        const customStylesheetLink = document.querySelector(
+        const userStylesheetLink = document.querySelector(
             'link[href^="data:text/css"]'
         );
 
         // If the css is found, remove the stylesheet
-        if (customStylesheetLink) {
-            customStylesheetLink.parentNode.removeChild(customStylesheetLink);
+        if (userStylesheetLink) {
+            userStylesheetLink.parentNode.removeChild(userStylesheetLink);
         }
     });
 });
