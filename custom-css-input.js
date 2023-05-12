b3f9865e4abd8d642c41420f74ad3f0ddccfb78f
sangmort <22422853+sangmort@users.noreply.github.com>
2023-05-12 12:01:01 -0400

    Add event listener for JS css upload

--- custom-css-input.js
+++ custom-css-input.js
@@ -1,38 +1,42 @@
-const fileInput = document.getElementById("file-input");
+document.addEventListener("DOMContentLoaded", () => {
+    const fileInput = document.getElementById("file-input");
+    let css;
 
-fileInput.addEventListener("change", (event) => {
-    const file = event.target.files[0];
+    fileInput.addEventListener("change", (event) => {
+        const file = event.target.files[0];
 
-    const reader = new FileReader();
-    reader.readAsText(file);
+        const reader = new FileReader();
+        reader.readAsText(file);
 
-    reader.onload = (event) => {
-        // Store content of CSS file in a variable
-        const css = event.target.result;
-    };
-});
+        reader.onload = (event) => {
+            css = event.target.result;
+        };
+    });
 
-const applyStylesButton = document.getElementById("apply-styles");
+    const applyStylesButton = document.getElementById("apply-styles");
 
-applyStylesButton.addEventListener("click", () => {
-    const cssDataUri = "data:text/css;charset=utf-8," + encodeURIComponent(css);
+    applyStylesButton.addEventListener("click", () => {
+        if (css) {
+            const cssDataUri = "data:text/css;charset=utf-8," + encodeURIComponent(css);
 
-    const customStylesheetLink = document.createElement("link");
-    customStylesheetLink.setAttribute("rel", "stylesheet");
-    customStylesheetLink.setAttribute("href", cssDataUri);
+            const customStylesheetLink = document.createElement("link");
+            customStylesheetLink.setAttribute("rel", "stylesheet");
+            customStylesheetLink.setAttribute("href", cssDataUri);
 
-    const head = document.getElementsByTagName("head")[0];
-    head.appendChild(customStylesheetLink);
-});
+            const head = document.getElementsByTagName("head")[0];
+            head.appendChild(customStylesheetLink);
+        }
+    });
 
-const removeStylesButton = document.getElementById("remove-styles");
+    const removeStylesButton = document.getElementById("remove-styles");
 
-removeStylesButton.addEventListener("click", () => {
-    const customStylesheetLink = document.querySelector(
-        'link[href^="data:text/css"]'
-    );
+    removeStylesButton.addEventListener("click", () => {
+        const customStylesheetLink = document.querySelector(
+            'link[href^="data:text/css"]'
+        );
 
-    if (customStylesheetLink) {
-        customStylesheetLink.parentNode.removeChild(customStylesheetLink);
-    }
+        if (customStylesheetLink) {
+            customStylesheetLink.parentNode.removeChild(customStylesheetLink);
+        }
+    });
 });
